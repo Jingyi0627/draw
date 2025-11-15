b0c77aaa9ff48ec8d612443831ddb20512f730fd
Jing <jingyi.wang01@sjsu.edu>
2025-11-14 18:30:41 -0800

    1

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let socket;
+
+function setup() {
+    createCanvas(600, 400);
+    background(0);
+
+    socket = io.connect('http://localhost:3000');
+
+    // 
+    socket.on('mouse', (data) => {
+        fill(255);
+        noStroke();
+        ellipse(data.x, data.y, 10, 10);
+    });
+}
+
+function mouseDragged() {
+    fill(255);
+    noStroke();
+    ellipse(mouseX, mouseY, 10, 10);
+
+    // 
+    const data = {
+        x: mouseX,
+        y: mouseY
+    };
+
+    socket.emit('mouse', data);
+}
